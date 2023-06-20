aac9330c92b64e7dce386fa1c5aff0fe72adfa5e
warhammer327 <warhammer327@pop-os.localdomain>
2023-06-21 00:05:03 +0600

    minor change

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,6 +1,8 @@
 class MembersController < ApplicationController
     before_action :authenticate_user!
     def index
+        #@users = User.all
+        #render json: @users, status: :ok
         render json: current_user, status: :ok
     end
 end

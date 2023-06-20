4468ebf2822fc9c8dbf38dc064e1d04451f29436
rizwan <rizwansm@outlook.com>
2023-06-20 17:21:37 +0600

    defined roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
   has_many :companies
+
+  ROLES = %w{super_admin admin manager editor collaborator}
+
+  ROLES.each do |role_name|
+    define_method "#{role_name}" do
+      role == role_name
+    end
+  end
+  
   def jwt_payload
     super
   end

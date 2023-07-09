83f76b7382cf98609510b5d7e3f05fa5d5e86361
warhammer327 <warhammer327@pop-os.localdomain>
2023-07-09 15:37:27 +0600

    aded rate limiting to specific endpoints

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,9 +1,18 @@
 class Rack::Attack
-    Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new 
-  
+    Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+
+    #Rack::Attack.throttle "logins/ip", limit: 20, period: 1 do |req|
+    #  req.post? && req.path == "/api-docs" && req.ip
+    #end
+
     throttle('api/ip', limit: 3, period: 10) do |req|
-      req.ip 
+      if req.path.match?(/^\/api\/v1\/companies$/i) && req.get?
+        req.ip
+      elsif  req.path.match?(/^\/api\/v1\/companies\/\d+$/) && req.patch?
+        req.ip
+      elsif  req.path.match?(/^\/api\/v1\/companies\/\d+$/) && req.delete?
+        req.ip
+      end
     end
-  
-    
+
   end
